83b935fa35350000038a0d82cf63a4f3cea80e16
Smack <Smack@Macintosh.local>
2018-02-24 13:30:04 -0500

    added geocodeLatLong

--- public/assets/js/getUserLocation.js
+++ public/assets/js/getUserLocation.js
@@ -78,7 +78,49 @@ function geocode(address) {
     });
 };
 
+// takes in a lat, long to give us our geoData object
+function geocodeLatLong(lat, long) {
+    event.preventDefault();
+
+    console.log("geocode: " + lat, long);
+
+    var queryURL = "http://maps.googleapis.com/maps/api/geocode/json?latlng=";
+
+    queryURL += lat + "," + long;
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).done(function (response) {
+        var status = response.status;
+        if (status === "OK") {
+
+            var resultRef = response.results[0];
+            var place_id = resultRef.place_id;
+            var formatted_address = resultRef.formatted_address;
+
+            var locationRef = resultRef.geometry.location;
+            var lat = locationRef.lat;
+            var lng = locationRef.lng;
+
+            return geoData = {
+                "lat": lat,
+                "lng": lng,
+                "place_id": place_id,
+                "address": formatted_address
+            };
+
+
+
+        } else {
+            alert('Geocode Lat Long unsuccessful.');
+        }
+    });
+};
+
+
 module.exports = {
     geocode,
-    geolocate
+    geolocate,
+    geocodeLatLong
 };
