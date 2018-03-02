f4f671b6b1a1f43178ab3cdd273933e92006638e
Aaron <aaronl.brinkley@gmail.com>
2018-03-01 20:37:27 -0500

    Added Animations

--- public/assets/js/viewingConditions.js
+++ public/assets/js/viewingConditions.js
@@ -9,10 +9,9 @@ function getViewingConditions(arr) {
 
        
 
-        // var from = new Date(res.from);
-        // var to = new Date(res.to);
-        // var from = from.toDateString()
-        // var to = to.toDateString()
+       
+        var resCloudCoverage = res.forecast.clouds.all
+        var resDesc = res.forecast.weather[0].description;
 
         var cloudCoverage = "Cloud coverage: " + res.forecast.clouds.all + "% ";
         var desc = "Conditions: " + res.forecast.weather[0].description;
@@ -20,49 +19,46 @@ function getViewingConditions(arr) {
         
         var image = $("<img>");
         var viewDiv = $("<div>");
-        // var fromDiv = $("<li>");
-        // var toDiv = $("<li>");
         var cloudDiv = $("<h4>");
         var descDiv = $("<h4>");
-
-        // viewDiv.empty()
 
 
         
         viewDiv.addClass("row col-12");
         viewDiv.addClass("viewCardClass");
-        // fromDiv.text("From: " + from);
-        // toDiv.text("To: " + to);
         cloudDiv.text(cloudCoverage);
         descDiv.text(desc);
 
-        // $(".viewCardClass").empty()
+        
 
         // if (image.attr("src") === "") {
         //     image.attr("src", "assets/images/random-image.jpg");
         // }
 
-        // if (difficultyDiv.text() === "green"){
-        //     difficultyDiv.html('<h5><img src="assets/images/diffGreen30.svg" id="difficultyImg"></h5>');
-        // }
-        // if (difficultyDiv.text() === "greenBlue"){
+        if (resCloudCoverage === 0){
+            cloudDiv.html('<div class="icon sunny"><div class="sun"><div class="rays"></div></div></div>');
+        }
+
+        if (resCloudCoverage === 50){
+            cloudDiv.html('<div class="icon cloudy"><div class="cloud"></div><div class="cloud"></div></div>');
+        }
+        // if (resCloudCoverage === "greenBlue"){
         //     difficultyDiv.html('<h5><img src="assets/images/diffGreenBlue30.svg" id="difficultyImg"></h5>');
         // }
-        // if (difficultyDiv.text() === "blue"){
+        // if (resCloudCoverage === "blue"){
         //     difficultyDiv.html('<h5><img src="assets/images/diffBlue30.svg" id="difficultyImg"></h5>');
         // }
-        // if (difficultyDiv.text() === "blueBlack"){
+        // if (resCloudCoverage === "blueBlack"){
         //     difficultyDiv.html('<h5><img src="assets/images/diffBlueBlack30.svg" id="difficultyImg"></h5>');
         // }
-        // if (difficultyDiv.text() === "black"){
+        // if (resCloudCoverage === "black"){
         //     difficultyDiv.html('<h5><img src="assets/images/diffBlack30.svg" id="difficultyImg"></h5>');
         // }
-        // if (difficultyDiv.text() === "dblack"){
+        // if (resCloudCoverage === "dblack"){
         //     difficultyDiv.html('<h5><img src="assets/images/diffBlack30.svg" id="difficultyImg"></h5>');
         // }
 
-        // viewDiv.append(fromDiv);
-        // viewDiv.append(toDiv);
+        
         viewDiv.append(cloudDiv);
         viewDiv.append(descDiv);
 
