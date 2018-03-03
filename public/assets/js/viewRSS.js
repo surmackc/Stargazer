19e13ca2d64eba441c8caa7868b182eb66e34bec
Tristan Benedict-Hall <tbenedicthall@Tristans-MacBook-Pro.local>
2018-03-02 19:27:12 -0500

    changed styling to viewrss, added geojson data packet to db folder, it contains points on map

--- public/assets/js/viewRSS.js
+++ public/assets/js/viewRSS.js
@@ -6,16 +6,19 @@ function viewRSS(arr) {
         if (!res.title.includes("days ago")) {
 
 
-
+            $(res.description).attr("id", "celEventDesc");
             var title = res.title;
             var desc = res.description;
             var link = res.link;
     
             var viewDiv = $("<div>");
             var titleDiv = $("<h4>");
+            $(titleDiv).attr("id", "celEventTitle");
+
             var linkDiv = $("<a href=" + link + " target='_blank'>Link to Article</a>");
     
             viewDiv.addClass("row col-12");
+            viewDiv.attr("class", "celEvent");
             titleDiv.text(title);
 
             viewDiv.append(titleDiv);
