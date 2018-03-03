0c03a1cc37a4e79d7749f6d06967993c003effcb
Smack <surmackc@gmail.com>
2018-03-02 19:46:25 -0500

    moon phase and date

--- public/assets/js/riseSetTimes.js
+++ public/assets/js/riseSetTimes.js
@@ -1,17 +1,26 @@
 function viewRiseSetTimes(res) {
     $('#moonPhaseText').empty();
 
+    
+    var closestPhase = res[0].closestphase.phase;
+    var closestDate = res[0].closestphase.date;
     var rise = res[0].moondata[2].time;
     var set = res[0].moondata[1].time;
 
     var viewDiv = $("<div>");
+    var phaseDiv = $("<h3>");
+    var dateDiv = $("<h3>");
     var riseDiv = $("<h3>");
     var setDiv = $("<h3>")
 
     viewDiv.addClass("row col-12");
-    riseDiv.text("Moonrise: " + rise);
-    setDiv.text("Moonset: " + set);
+    phaseDiv.text("Closest moon phase: " + closestPhase);
+    dateDiv.text("Occurrance: " + closestDate);
+    riseDiv.text("Moonrise Today: " + rise);
+    setDiv.text("Moonset Today: " + set);
 
+    viewDiv.append(phaseDiv);
+    viewDiv.append(dateDiv);
     viewDiv.append(riseDiv);
     viewDiv.append(setDiv);
 
